33665f8ebd23a2c0bdff58e7ccd16607548ec266
b251940123 <b251940123@erdenetis.edu.mn>
2026-05-18 21:42:27 +0800

    Refactor Student class constructors and methods
    Removed commented lines and added a blank line for clarity.

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Static method – объекттой холбоотой нийтлэг мэдээлэл
         Student.displaySchoolInfo();
         System.out.println();
-
-        // Default constructor ашиглан объект үүсгэх
+        
         Student student1 = new Student();
         System.out.println("--- Default constructor ---");
         student1.displayInfo();
 
-        // Parameterized constructor ашиглан объект үүсгэх
         Student student2 = new Student("Болд", "Баатар", 20, "Мэдээллийн технологи", 92.5);
         System.out.println("--- Parameterized constructor ---");
         student2.displayInfo();
